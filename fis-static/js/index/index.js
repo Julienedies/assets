49a91510dfaecf1648651f08c4969e553de10ebe
Julienedies <julienedies@gamil.com>
2015-03-11 15:50:50 +0800

    deleted more

--- fis-static/js/index/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by julien.zhang on 2014/9/28.
- */
-__inline('ctrl.js');
-__inline('model.js');
-console.log('fis test!!!!!!!!!!!!!!')
--- fis-static/js/index/model.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by julien.zhang on 2014/9/28.
- */
-function model(){
-    alert('model');
-}
